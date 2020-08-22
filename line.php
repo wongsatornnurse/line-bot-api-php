70bbbd673d504da5892fb19607eb1a3f554be9d5
wongsatornnurse <70052037+wongsatornnurse@users.noreply.github.com>
2020-08-22 16:17:03 +0700

    Update line.php

--- line.php
+++ line.php
@@ -38,7 +38,7 @@ if ( sizeof($request_array['events']) > 0 )
      		$obj = json_decode($result);
      
      		//$reply_message = $result;
-    		 	$reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .'คน' "\r\n" 'รักษาหายแล้ว '.$obj->{'Recovered'} .'คน';
+    		 	$reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .'คน รักษาหายแล้ว '.$obj->{'Recovered'} .'คน';
     		}
 		//$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
    }
