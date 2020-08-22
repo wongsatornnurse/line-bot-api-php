2bb5cd72578916591cec6f172ed51d31caf866c2
wongsatornnurse <70052037+wongsatornnurse@users.noreply.github.com>
2020-08-22 15:31:22 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'MI5/Zsb0N/oB/oBGw0V7hh8UhCvWMbCANwXkd+2mSHwPIxhnvww8EKjWzvfE7Q9OeivgZj8B32ojTXYKMUuptQB5EzSX89MiBnOzP/Yv9AVvjUxQgoQkeMeBjhJBG+tOPk+LxrGdaIqhzmtZJODiHQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = '/Xessh2CXHnQKq0QXOiSP+uLphllCbSTyMfRhISTsWcUzL/60fiynZj7+bjgSCdg8F5igS3WdTvup+JuEbfD174LDr1giNM7g3BPcf0kzjPkFpHZ5Nwrdckrjuj4k2Dx2zvb+wJnlXF+yUuGlBnhggdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
